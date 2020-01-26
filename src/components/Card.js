d628ab982e0d3accdcf339584a3bf601458e32c4
alexanderturinske <a.turinske@gmail.com>
2020-01-26 15:20:34 -0700

    Implement cursor for text

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 
-function Card({ searchTerm }) {
-  return (
-    <div className='Card'>
-      <div>{searchTerm}</div>
-    </div>
-  );
+class Card extends Component {
+  componentDidMount() {
+    // https://stackoverflow.com/questions/28889826/set-focus-on-input-after-render
+    this.nameInput.focus();
+  }
+
+  render() {
+    const { searchTerm } = this.props;
+    return (
+      <div className='card'>
+        <input
+          className="card__input"
+          ref={(input) => { this.nameInput = input; }}
+          type="text"
+          value={searchTerm}
+        />
+      </div>
+    );
+  }
 }
 
 export default Card;
