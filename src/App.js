5dad88691227556884db18df637cca12b2fc50c0
alexanderturinske <a.turinske@gmail.com>
2020-01-26 14:46:09 -0700

    Show all animal names in cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Card } from './components'
+import animals from './assets/animal_names.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 1
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {
+          animals.map(animal => (
+            <Card searchTerm={animal} />
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
