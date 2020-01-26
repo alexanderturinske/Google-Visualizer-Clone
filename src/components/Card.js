28405eb082e773a5caf9c67616a7a72c1e0d88da
alexanderturinske <a.turinske@gmail.com>
2020-01-26 16:01:54 -0700

    Implement typing completing

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,27 @@ class Card extends Component {
     // https://stackoverflow.com/questions/28889826/set-focus-on-input-after-render
     this.nameInput.focus();
 
-    const typing = setInterval(() => {
+    const interval = setInterval(() => {
+      const currentLength = this.nameInput.value.length;
+      const searchTermLength = this.props.searchTerm.length;
+      if (currentLength === searchTermLength) {
+        clearCard();
+      }
       // take the length of the current value, add one, and grab the first n characters
       // from the search term
       this.nameInput.value = this.props.searchTerm.substring(0, this.nameInput.value.length + 1);
-    }, 2000);
+    }, 1000);
+
+    const clearCard = this.clearCard(interval);
+  }
+
+  clearCard(interval) {
+    return function() {
+      clearInterval(interval);
+      setTimeout(() => {
+        console.log('cleared');
+      }, Math.random() * 2);
+    }
   }
 
   render() {
